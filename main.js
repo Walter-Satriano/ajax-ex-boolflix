a78be931807492ff96bf25c82647e7fd4bf6c543
Walter Satriano <walter.satri@gmail.com>
2019-08-22 19:03:24 +0200

    aggiunta funzione per visualizzare le bandiere in base alla lingua originale

--- main.js
+++ main.js
@@ -1,33 +1,24 @@
-/*Milestone 1:
-Creare un layout base con una searchbar (una input e un button) in cui possiamo
-scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il
-bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni
-film trovato:
-1. Titolo
-2. Titolo Originale
-3. Lingua
-4. Voto*/
+/*ESERCIZIO BOOLFLIX*/
 
 
 $(document).ready(function() {
-
 
   //compilo il template con handlebars
   var source = $("#template").html();
   var template = Handlebars.compile(source);
 
 
+
   $("#search_button").click(
     function() {
-
-
       //salvo l'input utente in una variabile
       var inputUser = $(".input_user").val().toUpperCase();
       console.log(inputUser);
-
       //svuoto l'input
       $(".input_user").val("");
+
+      //pulisco il campo ad ogni ricerca per non accavallare i risultati di più ricerche
+      $(".container_movie").empty();
 
       //chiamata ajax in base all'input dell'utente
       $.ajax(
@@ -44,7 +35,6 @@ $(document).ready(function() {
             var movie = data.results;
             console.log(movie);
 
-
             //ciclo per traversare l'array ottenuto dall'API
             for (var i = 0; i < movie.length; i++) {
               var eachMovie = movie[i];
@@ -60,7 +50,8 @@ $(document).ready(function() {
                 title: eachMovie.title,
                 original_title: eachMovie.original_title,
                 original_language: eachMovie.original_language,
-                vote_average: vote1to5
+                vote_average: vote1to5,
+                overview: eachMovie.overview
               };
 
               var html = template(context);
@@ -70,6 +61,7 @@ $(document).ready(function() {
             }
 
             ratingStar();
+            flag();
           },
           error : function(richiesta, stato, errore) {
             alert("E' avvenuto un errore. " + errore);
@@ -77,7 +69,7 @@ $(document).ready(function() {
         }
       );
     }
-  ); //fine metodo click
+  ); //fine funzione al click
 
 
   //funzione per creare la votazione con le stelle
@@ -85,7 +77,7 @@ $(document).ready(function() {
     $(".rate_star").each(
       function() {
 
-        var num = $(this).attr("data-numero");
+        var num = $(this).attr("data-vote");
         var diff = 5 - num;
         //stelle piene
         for (var i = 0; i < num; i++) {
@@ -94,6 +86,66 @@ $(document).ready(function() {
         //stelle vuote
         for (var i = 0; i < diff; i++) {
           $(this).append("<i class='far fa-star'></i>");
+        }
+      }
+    )
+  }
+
+  //funzione per le bandiere delle Lingue principali
+  function flag() {
+    $(".flag_lang").each(
+      function() {
+
+        var lang = $(this).attr("data-flag")
+
+        // casi bandiera divisi per linguaggio (principali)
+        switch (lang) {
+          case "en":
+            $(this).html("<img src='img/en-flag.png'>")
+            break;
+
+          case "it":
+            $(this).html("<img src='img/it-flag.png'>")
+            break;
+
+          case "cn":
+            $(this).html("<img src='img/cn-flag.png'>")
+            break;
+
+          case "de":
+            $(this).html("<img src='img/de-flag.png'>")
+            break;
+
+          case "es":
+            $(this).html("<img src='img/es-flag.png'>")
+            break;
+
+          case "fr":
+            $(this).html("<img src='img/fr-flag.png'>")
+            break;
+
+          case "in":
+            $(this).html("<img src='img/in-flag.png'>")
+            break;
+
+          case "jp":
+            $(this).html("<img src='img/jp-flag.png'>")
+            break;
+
+          case "pt":
+            $(this).html("<img src='img/pt-flag.png'>")
+            break;
+
+          case "ru":
+            $(this).html("<img src='img/ru-flag.png'>")
+            break;
+
+          case "usa":
+            $(this).html("<img src='img/usa-flag.png'>")
+            break;
+
+          default:
+            $(this).html("n/d")
         }
       }
     )
